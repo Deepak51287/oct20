98e81314f0f0d934cb41f846c2b884649b5536e2
Rahul <Rahul@Home>
2021-10-20 20:44:30 +0530

    script2 updated

--- Oct21/src/P1/Demo2.java
+++ Oct21/src/P1/Demo2.java
@@ -2,8 +2,9 @@ package P1;
 
 public class Demo2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		//hi hello
 
 	}
 
